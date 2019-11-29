ee4fb64d194f6e8dc9ebd64774c0da6bda78d7c9
traviszeng <traviszeng7@gmail.com>
2019-11-29 16:46:47 +0800

    添加可以使用catboost xgboost lightgbm的方法

---
+++ EnsembleMethod.py
@@ -0,0 +1,113 @@
+import xgboost as xgb
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from catboost import CatBoostRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+
+"""
+    A method which can be used for sklearn library training and xgboost, lightgbm and catboost
+"""
+n_fold = 10 #10折交叉验证
+folds = KFold(n_splits = n_fold,shuffle = True,random_state = 42)
+def train_model(X, X_test, y, params=None, folds=folds,
+                model_type='lgb', plot_feature_importance=False, model=None):
+    oof = np.zeros(X.shape[0])  # 生成一个跟X行数一样长的零矩阵
+    prediction = np.zeros(X_test.shape[0])
+    scores = []
+    feature_importance = pd.DataFrame()  # 生成空矩阵
+    for fold_n, (train_index, valid_index) in enumerate(folds.split(X)):
+        print('Fold', fold_n, 'started at', time.ctime())
+        if model_type == 'sklearn':
+            X_train, X_valid = X[train_index], X[valid_index]
+        else:
+            X_train, X_valid = X.values[train_index], X.values[valid_index]
+        y_train, y_valid = y[train_index], y[valid_index]
+
+        if model_type == 'lgb':
+            """
+                #**动态参数 
+                n_estimators=100: 要拟合的树的棵树，可以认为是训练轮数 
+                n_jobs=1: 并行运行的多线程数 
+                nthread [默认为未设置的最大线程数]并行线程数
+                """
+            model = lgb.LGBMRegressor(**params, n_estimators=20000, nthread=4, n_jobs=-1)
+            """
+                #eval_metric: 评价指标 rmse - 均方根误差 
+                early_stopping_rounds: 提前结束轮数 
+                eval_set:训练集与测试集 
+                verbose=1000使用详细日志记录级别并将日志记录周期设置为此参数的值
+                """
+            model.fit(X_train, y_train,
+                      eval_set=[(X_train, y_train), (X_valid, y_valid)], eval_metric='rmse',
+                      verbose=1000, early_stopping_rounds=200)
+
+            y_pred_valid = model.predict(X_valid)
+            y_pred = model.predict(X_test, num_iteration=model.best_iteration_)
+
+        if model_type == 'xgb':
+            train_data = xgb.DMatrix(data=X_train, label=y_train)  # 使用XGBoost的原生版本需要对数据进行转化
+            valid_data = xgb.DMatrix(data=X_valid, label=y_valid)
+
+            watchlist = [(train_data, 'train'), (valid_data, 'valid_data')]
+            model = xgb.train(dtrain=train_data, num_boost_round=20000, evals=watchlist, early_stopping_rounds=200,
+                              verbose_eval=500, params=params)
+            y_pred_valid = model.predict(xgb.DMatrix(X_valid), ntree_limit=model.best_ntree_limit)
+            y_pred = model.predict(xgb.DMatrix(X_test.values), ntree_limit=model.best_ntree_limit)
+
+        if model_type == 'sklearn':
+            model = model
+            model.fit(X_train, y_train)
+            y_pred_valid = model.predict(X_valid).reshape(-1, )
+            score = mean_squared_error(y_valid, y_pred_valid)
+
+            y_pred = model.predict(X_test)
+
+        if model_type == 'cat':
+            model = CatBoostRegressor(iterations=20000, eval_metric='RMSE',
+                                      **params)
+            model.fit(X_train, y_train, eval_set=(X_valid, y_valid), cat_features=[], use_best_model=True,
+                      verbose=False)
+
+            y_pred_valid = model.predict(X_valid)
+            y_pred = model.predict(X_test)
+
+        oof[valid_index] = y_pred_valid.reshape(-1, )  # reshape(-1,)转换成一行 将对valid的预测结果存成矩阵
+        scores.append(mean_squared_error(y_valid, y_pred_valid) ** 0.5)  # 每交叉验证一次得到一个loss分数，一共有n_fold个值
+
+        prediction += y_pred  # 每次预测的结果累加 可能需要.reshape(-1,)
+
+        if model_type == 'lgb':
+            # feature importance
+            fold_importance = pd.DataFrame()
+            fold_importance["feature"] = X.columns
+            fold_importance["importance"] = model.feature_importances_
+            fold_importance["fold"] = fold_n + 1  # 第几次交叉验证的标志
+            feature_importance = pd.concat([feature_importance, fold_importance], axis=0)  # 将每次交叉验证的结果得到的变量importance保存
+
+    prediction /= n_fold
+
+    print('CV mean score: {0:.4f}, std: {1:.4f}.'.format(np.mean(scores), np.std(scores)))
+
+    if model_type == 'lgb':
+        feature_importance["importance"] /= n_fold
+        if plot_feature_importance:
+            cols = feature_importance[["feature", "importance"]].groupby("feature").mean().sort_values(
+                by="importance", ascending=False)[:50].index
+
+            best_features = feature_importance.loc[feature_importance.feature.isin(cols)]
+
+            plt.figure(figsize=(16, 12));
+            sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False));
+            plt.title('LGB Features (avg over folds)');
+
+            return oof, prediction, feature_importance
+        return oof, prediction
+
+    else:
+        return oof, prediction
